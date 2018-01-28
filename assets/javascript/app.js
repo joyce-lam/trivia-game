342c60ad3a6becedf291a216b38410b0d2763250
Joyce <joycelamsaungan@gmail.com>
2018-01-27 16:23:45 -0800

    adding questions to app.js and modifying index.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+//trivia questions
+var questions = [{
+	question: "Which city in Australia has the highest population?",
+	choice: ["Sydney", "Canberra", "Brisbane", "Perth"],
+	answer: "Sydney"
+	}, {
+	question: "Which is the capital city of Australia?",
+	choice: ["Canberra", "Perth", "Sydney", "Brisbane"],
+	answer: "Canberra"	
+	}, {
+	question: "A koala's diet consists mainly of what?",
+	choice: ["Eucalyptus leaves", "Smaller koalas", "Bamboo", "Avocado toast"],
+	answer: "Eucalyptus leaves"
+	}, {
+	question: "What is the name of the popular Australian food spread used on sandwiches, toast and pastries?",
+	choice: ["Kaya", "Vegemite", "Skippy's", "Daiya"],
+	answer: "Vegemite"
+	}, {
+	question: "What is a dingo?",
+	choice: ["Flower", "Tree", "Bird", "Wild dog"],
+	answer: "Wild dog"
+	}, {
+	question: "Which is not a common name for kangaroo?",
+	choice: ["Buck", "Boomer", "Bandicoot", "Old Man"],
+	answer: "Bandicoot"
+	}]
+
+
+
+
