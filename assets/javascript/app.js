cc345c173bb1ee3beeed31e5eff417d2dab59975
Joyce <joycelamsaungan@gmail.com>
2018-01-27 17:46:46 -0800

    modifying app.js with added functions to show questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,27 @@
 //trivia questions
 var questions = [{
 	question: "Which city in Australia has the highest population?",
-	choice: ["Sydney", "Canberra", "Brisbane", "Perth"],
+	choices: ["Sydney", "Canberra", "Brisbane", "Perth"],
 	answer: "Sydney"
 	}, {
 	question: "Which is the capital city of Australia?",
-	choice: ["Canberra", "Perth", "Sydney", "Brisbane"],
+	choices: ["Canberra", "Perth", "Sydney", "Brisbane"],
 	answer: "Canberra"	
 	}, {
 	question: "A koala's diet consists mainly of what?",
-	choice: ["Eucalyptus leaves", "Smaller koalas", "Bamboo", "Avocado toast"],
+	choices: ["Eucalyptus leaves", "Smaller koalas", "Bamboo", "Avocado toast"],
 	answer: "Eucalyptus leaves"
 	}, {
 	question: "What is the name of the popular Australian food spread used on sandwiches, toast and pastries?",
-	choice: ["Kaya", "Vegemite", "Skippy's", "Daiya"],
+	choices: ["Kaya", "Vegemite", "Skippy's", "Daiya"],
 	answer: "Vegemite"
 	}, {
 	question: "What is a dingo?",
-	choice: ["Flower", "Tree", "Bird", "Wild dog"],
+	choices: ["Flower", "Tree", "Bird", "Wild dog"],
 	answer: "Wild dog"
 	}, {
 	question: "Which is not a common name for kangaroo?",
-	choice: ["Buck", "Boomer", "Bandicoot", "Old Man"],
+	choices: ["Buck", "Boomer", "Bandicoot", "Old Man"],
 	answer: "Bandicoot"
 	}]
 
@@ -37,11 +37,32 @@ function startGame() {
 }
 
 function showQuestion() {
+	var questionGroups = $('#question-groups');
 
+	for (var i = 0; i<questions.length; i++) {
+		var questionGroup = $('<div>');
 
+		var questionDiv = $('<div>');
+		questionDiv.addClass(questions);
+		questionDiv.data("q", i);
+		questionDiv.html(questions[i].question);
+		questionGroup.append(questionDiv);
 
+		for (var j = 0; j < questions[i].choices.length; j++) {
+			var choiceBtn = $('<button>');
+			choiceBtn.addClass("btn btn-success");
+			choiceBtn.data("value", j);
+			choiceBtn.text(questions[i].choices[j]);
+			questionGroup.append(choiceBtn);
 
-	
+			console.log(choiceBtn.data("value"));
+		}
+
+		questionGroups.append(questionGroup);
+
+	}
+
+		countdown();
 }
 
 function countdown() {
